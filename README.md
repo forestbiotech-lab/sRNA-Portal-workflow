3bbe5e1473cddff1bfe01953539523e7e0ec0dce
Bruno Costa <netbofia@hotmail.com>
2017-01-30 15:20:24 +0000

    Added modal, new config files. Etc. Testing getting terms from ontologies.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 This will be a portal that implements a Database to store results from miRPursuit. As well as additional services that can run on top of the database.
 
 Check out more information on this <a href="https://github.com/forestbiotech-lab/sRNA-Portal-workflow/blob/master/sRNAportal.pdf"/>here</a>
+
+
+## Run miRPursuit
+
+Upload files
+
+Annotate them 
+
+Analyse data
+
+View results
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var formidable = require('formidable')
 var fs = require('fs');
 var path = require('path');
 var miRPursuit = require('./../views/strings/miRPursuit.json') 
+var config = require('./../config/ontologies.json') 
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +16,34 @@ router.get('/', function(req, res, next) {
 router.get('/miRPursuit', function(req, res, next) {
   res.render('miRPursuit', {lang:"en",text: miRPursuit.en});
 });
+
+router.post('/search-term',function(req,res){
+	//console.log(req);
+	var form = new formidable.IncomingForm();
+	form.encoding = 'utf-8';
+	form.parse(req, function(err,fields, files){
+		console.log(fields);
+		//Get term from specific id
+
+
+
+	})
+	console.log(form);
+	var options = {
+		host: config.termSearch.bioportal.HOST,
+		port: '80',
+		path: config.termSearch.bioportal.PATH,
+		method: 'POST',
+		headers: {
+			'Authorization': config.Authorization+"token",//+process.env.agr
+			'Content-Type': 'application/x-www-form-urlencoded'//,
+    		//'Content-Length': post_data.length
+		}
+
+	}
+	console.log(options);
+	res.send('ok');
+})
 
 router.post('/upload', function(req, res){
   // create an incoming form object
