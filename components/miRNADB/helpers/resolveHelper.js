b41c0da4288f2eedc350286c7e9bc45a0fadd760
Bruno Costa <netbofia@hotmail.com>
2018-10-02 16:55:35 +0100

    Cleans up resolveHelper and add info on function

--- components/miRNADB/helpers/resolveHelper.js
+++ components/miRNADB/helpers/resolveHelper.js
@@ -1,13 +1,28 @@
 var getOptions=require('./getOptions');
 
+
+/**
+* @ resolveCall -  function to query the database for a given call
+                   and process the result.
+*
+* @params call - The call to retrieve the data
+* @params req - Express router, request
+* @params res - Express router, result
+* @params errMsg - Message to be displayed if an error is caught
+* @params [view] Optional - Give the json to a view instead
+*
+*/
+
+
 function resolveCall(call,req,res,errMsg,view){
 
   var options=getOptions(req);
   call(options).then(function(callRes){
     if (view){
-      console.log(callRes.result.data[0].Trial)
+
       res.render(view,callRes)
     }else{
+      console.log(callRes.result.data)
       res.status(200).json(callRes);
     }
   }).catch(function(err){
