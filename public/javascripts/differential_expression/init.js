ecaea551e8dc7c413046cc4126225cb3143e0296
Bruno Costa <brunocosta@itqb.unl.pt>
2019-11-25 17:56:08 +0000

    #Fix #9 adds upload capacity

---
+++ public/javascripts/differential_expression/init.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  scriptName=[
+    'ajax',
+//    'loaders',
+//  'setters',
+ //   'getters',
+ //   'DOMbasics',
+ //   'listeners'
+  ]
+
+
+  scriptTarget = $('script#actions')[0];
+  for (s in scriptName){
+    let url="/javascripts/differential_expression/"+scriptName[s]+".js";
+    let script = document.createElement('script');
+    script.src = url;
+    scriptTarget.parentNode.insertBefore(script, scriptTarget);
+  }
+
+
+
+});
+
+
+
+
+
---
+++ routes/differential_expression.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable')
+var fs = require('fs')
+var path=require('path')
+var Promise = require("bluebird");
+
+router.get('/',function(req,res){
+	res.render('differential_expression');
+})
+
+//POST UPLOAD                                     POST UPLOAD    //RESTRICT access required
+router.post('/upload', function(req, res){
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  //Calculate file hash
+  form.hash = 'md5';
+  console.log(__dirname);
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '../uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's original name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name), function(){console.log("Rename done")});
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+  	file={hash:form.openedFiles[0].hash, name:form.openedFiles[0].name}
+    res.json(file);
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});
+
+module.exports = router;
