8b369204928009d9e67fec3b036236b9db38a69b
Bruno Costa <brunocosta@itqb.unl.pt>
2020-02-18 15:44:01 +0000

    #93 starts by detecting foreign keys

--- components/forms/formFromTable.js
+++ components/forms/formFromTable.js
@@ -2,12 +2,13 @@ var db=require('./../miRNADB/sqldb')
 var models=require('./models')
 
 let inputType={
-	INTEGER:"number",
-	FLOAT:"number",
-	STRING:"text",
-	DATE:"date",
-	BOOLEAN:"checkbox",
-  TEXT:"text"
+  INTEGER:"number",
+  FLOAT:"number",
+  STRING:"text",
+  DATE:"date",
+  BOOLEAN:"checkbox",
+  TEXT:"text",
+  FK:"select"
 }
 
 
@@ -15,26 +16,43 @@ let inputType={
 
 
 function ts(table){
-	let tableStructure=[]
-	if (db[table]==undefined) throw new Error(`This table [${table}] does not exist!`);
-	var tableAttributes=db[table].tableAttributes
-	Object.keys( tableAttributes ).forEach(function(attribute){
-		let type=tableAttributes[attribute].type.key
-		tableStructure.push({name:attribute,type:inputType[type]})
-	})
-	return tableStructure
+  let tableStructure=[]
+  if (db[table]==undefined) throw new Error(`This table [${table}] does not exist!`);
+  var associations=db[table].associations
+  var tableAttributes=db[table].tableAttributes
+  Object.keys( tableAttributes ).forEach(function(attribute){
+    fk=isAttrAFK(attribute,associations)
+    let type= fk.targetKey==null ? tableAttributes[attribute].type.key : "FK"
+    tableStructure.push({name:attribute, type:inputType[type], fk })
+  })
+  return tableStructure
 }
 
 function te(options){
-	call="getTableValueById"
-	return models[call](options)
+  call="getTableValueById"
+  return models[call](options)
 }
 
+function isAttrAFK(attr,associations){
+  let result={targetKey:null,targetTable:null,values:[]}
+  if(Object.keys(associations).length==0 || attr=='id'){
+    return result
+  }else{
+    Object.keys(associations).forEach(targetTable=>{
+      let options=associations[targetTable].options
+      if(options.foreignKey==attr){
+        result.targetKey=options.targetKey
+        result.targetTable=targetTable
+      }
+    })
+    return result
+  }
+}
 
 
 
 
 module.exports={
-	tableStructure:ts,
-	tableEntry:te,
+  tableStructure:ts,
+  tableEntry:te,
 }
--- routes/forms.js
+++ routes/forms.js
@@ -12,12 +12,18 @@ router.get('/factory/fromTable/basic/:table', function(req, res, next){
   table=getTableAttr.tableStructure(table)
   res.render('factory/basic-form',{table,name:req.params.table});
 })
+router.get('/factory/fromTable/fks/:table', function(req, res, next){
+  let table=req.params.table
+  table=getTableAttr.tableStructure(table)
+  //get values
+  res.render('factory/lookupFK-form',{table,name:req.params.table});
+})
 router.get('/factory/fromTable/byId/:table/:id',function(req,res){
   let tablename=req.params.table
   let id=req.params.id
   let options={tablename,where:{id}}
   getTableAttr.tableEntry(options).then(function(data){
-    data instanceof Error? res.render('error',{error:data}) : res.json(data);
+    data instanceof Error ? res.render('error',{error:data}) : res.json(data);
   }).catch(function(error){
     res.render('error',error)
   })
