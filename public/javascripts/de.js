2c9a32da9d82afc6fb080311f474015ad50f9b4b
Bruno Costa <brunovasquescosta@gmail.com>
2023-03-22 17:16:41 +0000

    updates loading of javascript files for diferent views

--- public/javascripts/de.js
+++
@@ -1,7 +0,0 @@
-//TODO deprecate will be progressivly removed
-
-let scriptTarget=document.getElementById('actions')
-let url="/javascripts/differential_expression/init.js";
-let script = document.createElement('script');
-script.src = url;
-scriptTarget.parentNode.insertBefore(script, scriptTarget);
